e39f934d6e2a528674800fbf12cdcb6c7c6e8bd7
Alvaro <aleteliert@gmail.com>
2018-08-03 17:51:58 -0400

    Varible de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds113122.mlab.com:13122/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
